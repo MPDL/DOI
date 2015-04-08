04b4c69379e8a93d33878efc4d005988d6a3be4c
haarli <haarlaender@mpdl.mpg.de>
2015-04-08 17:00:55 +0200

    Test commit

--- doxi/src/test/java/de/mpg/mpdl/doi/SimpleTest.java
+++ doxi/src/test/java/de/mpg/mpdl/doi/SimpleTest.java
@@ -2,6 +2,8 @@ package de.mpg.mpdl.doi;
 
 import javax.ws.rs.core.Application;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.glassfish.jersey.server.ResourceConfig;
 import org.glassfish.jersey.test.JerseyTest;
 import org.junit.Assert;
@@ -11,7 +13,7 @@ import de.mpg.mdpl.doi.MyResource;
 
 public class SimpleTest extends JerseyTest {
 	 
-
+	private Logger logger = LogManager.getLogger(SimpleTest.class);
  
     @Override
     protected Application configure() {
