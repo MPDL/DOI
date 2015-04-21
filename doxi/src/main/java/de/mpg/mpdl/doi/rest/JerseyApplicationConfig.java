68341d02419bea91c9fe5ed0b6c6f37ea71e2ba6
haarli <haarlaender@mpdl.mpg.de>
2015-04-21 15:02:10 +0200

    New spring config

--- doxi/src/main/java/de/mpg/mpdl/doi/rest/JerseyApplicationConfig.java
+++ doxi/src/main/java/de/mpg/mpdl/doi/rest/JerseyApplicationConfig.java
@@ -4,13 +4,8 @@ import javax.ws.rs.ApplicationPath;
 
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
+import org.glassfish.jersey.filter.LoggingFilter;
 import org.glassfish.jersey.server.ResourceConfig;
-import org.springframework.context.ApplicationContext;
-import org.springframework.web.context.ContextLoader;
-
-import de.mpg.mpdl.doi.rest.exceptionMapper.DoiAlreadyExistsMapper;
-import de.mpg.mpdl.doi.security.spring.config.SecurityConfig;
-import de.mpg.mpdl.doi.security.spring.config.SecurityWebApplicationInitializer;
 
 @ApplicationPath("/")
 public class JerseyApplicationConfig extends ResourceConfig {
@@ -18,11 +13,14 @@ public class JerseyApplicationConfig extends ResourceConfig {
 	Logger logger = LogManager.getLogger();
 	public JerseyApplicationConfig()
 	{
-		packages(true,"de.mpg.mpdl.doi");
-	    //property("contextConfigLocation", "classpath:applicationContext.xml");
 		
-		register(SecurityConfig.class);
-		register(SecurityWebApplicationInitializer.class);
+	    //property("contextConfigLocation", "classpath:applicationContext.xml");
+	    packages(true,"de.mpg.mpdl.doi");
+		registerInstances(new LoggingFilter(java.util.logging.Logger.getLogger("test"), true));
+		
+		
+		//register(SecurityConfig.class);
+		//register(SecurityWebApplicationInitializer.class);
 		
 		//ApplicationContext appCon = ContextLoader.getCurrentWebApplicationContext();
 		//logger.info(appCon);
--- doxi/src/main/java/de/mpg/mpdl/doi/security/spring/config/SecurityConfig.java
+++ doxi/src/main/java/de/mpg/mpdl/doi/security/spring/config/SecurityConfig.java
@@ -4,10 +4,12 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.stereotype.Component;
 
+@Configuration
 @EnableWebSecurity
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
@@ -16,5 +18,21 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 		auth
 			.inMemoryAuthentication()
 				.withUser("user").password("password").roles("USER");
+	
 	}
+	
+	
+	@Override
+	public void configure(HttpSecurity http) throws Exception {
+		
+		http
+			.authorizeRequests()
+			.anyRequest().authenticated()
+			.and()
+		.httpBasic()
+			.and()
+		.csrf().disable();
+	
+	}
+	
 }
