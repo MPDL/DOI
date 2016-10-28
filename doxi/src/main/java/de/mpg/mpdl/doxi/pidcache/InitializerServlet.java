4cab16c6d4c67ea1be8062545473bab1da011bc2
przMPDL <przibylla@mpdl.mpg.de>
2016-10-28 15:38:06 +0200

    some changes

--- doxi/src/main/java/de/mpg/mpdl/doxi/pidcache/InitializerServlet.java
+++ doxi/src/main/java/de/mpg/mpdl/doxi/pidcache/InitializerServlet.java
@@ -1,20 +1,10 @@
 package de.mpg.mpdl.doxi.pidcache;
 
-import java.sql.Driver;
-import java.sql.DriverManager;
-import java.sql.SQLException;
-import java.util.Enumeration;
-
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-
 @SuppressWarnings("serial")
 public class InitializerServlet extends HttpServlet {
-  private static final Logger LOG = LoggerFactory.getLogger(InitializerServlet.class);
-
   private PidCacheTask pidCacheTask;
   private PidQueueTask pidQueueTask;
 
@@ -33,24 +23,5 @@ public class InitializerServlet extends HttpServlet {
 
     this.pidCacheTask.interrupt();
     this.pidQueueTask.interrupt();
-
-//    // TODO sollte eigentlich JerseyApplicationConfig tun
-//    // Close factory - necessary for proper restart
-//    if (JerseyApplicationConfig.emf != null && JerseyApplicationConfig.emf.isOpen()) {
-//      LOG.info("closing EntityManagerFactory");
-//      JerseyApplicationConfig.emf.close();
-//    }
-
-    // TODO sollte eigentlich JerseyApplicationConfig tun
-    Enumeration<Driver> drivers = DriverManager.getDrivers();
-    while (drivers.hasMoreElements()) {
-      Driver driver = drivers.nextElement();
-      try {
-        DriverManager.deregisterDriver(driver);
-        LOG.info("deregistering jdbc driver: {}", driver);
-      } catch (SQLException e) {
-        LOG.error("Error deregistering driver: {}\n{}", driver, e);
-      }
-    }
   }
 }
