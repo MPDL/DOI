f891c939cce7fdc19ec73f2178dfa36a3c73f423
haarli <haarlaender@mpdl.mpg.de>
2017-02-13 17:07:43 +0100

    Updated metadata generator to version 4. Reordered main menu.

---
+++ doxi/src/main/webapp/resources/doxi.js
@@ -0,0 +1,27 @@
+		function logout() {
+			$.ajax({
+				type: "GET",
+				url: "/doxi/rest/doi",
+				dataType: 'json',
+				async: true,
+				username: "user",
+				password: "any_stupid_password"
+			})
+				
+			//In our case, we WANT to get access denied, so a success would be a failure.
+			.done(function(){
+				alert("Error while logging out");
+			})
+			
+			//Likewise, a failure *usually* means we succeeded.
+			//set window.location to redirect the user to wherever you want them to go
+			.fail(function(data){
+				if(data.status==401) {
+					alert("Logout successful");
+				}
+				else {
+					alert("Error while logging out");
+				}
+				//window.location = "/doxi";
+			});
+		}
